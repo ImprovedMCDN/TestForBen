0ce3b1c43e7ba98f9a8c91e28692dcb22b152df3
xF4ilSh0oter <benhero@ag1.ch>
2013-07-07 00:18:51 +0200

    Create Projekt

---
+++ TestForBen/src/me/xF4ilSh0oter/main/MainClass.java
@@ -0,0 +1,14 @@
+package me.xF4ilSh0oter.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MainClass extends JavaPlugin {
+	
+	public void onDisable() {
+		
+	}
+	
+	public void onEnable() {
+		
+	}
+}
